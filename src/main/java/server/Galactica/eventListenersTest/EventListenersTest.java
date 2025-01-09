f077a4c1420f00a22cbb908d0b2669cd1d717194
DinoBoy238 <spencerb.me>
2025-01-08 23:39:32 -0500

    This has the bed plugin part I was talking about

--- src/main/java/server/Galactica/eventListenersTest/EventListenersTest.java
+++ src/main/java/server/Galactica/eventListenersTest/EventListenersTest.java
@@ -6,6 +6,7 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -27,11 +28,25 @@ public final class EventListenersTest extends JavaPlugin implements Listener {
 
         if (targetPlayer != null && targetPlayer.isOnline()) {
             // Send the message to the target player
-            targetPlayer.sendMessage(ChatColor.BLUE + joinedPlayer.getName() + ChatColor.GOLD + "has joined" + ChatColor.AQUA + "GALATICASMP");
-            targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_CREEPER_PRIMED, 10.0f, 1.0f)
+            targetPlayer.sendMessage(ChatColor.BLUE + joinedPlayer.getName() + ChatColor.GOLD + " has joined " + ChatColor.AQUA + "GALATICASMP");
+            targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, 10.0f, 1.0f);
         } else {
             //Target player is not online
             System.out.println("Target player " + targetPlayerName + " is not online.");
+        }
+    }
+
+    @EventHandler
+    public void onBedEnter(PlayerBedEnterEvent event) {
+        Player enteringPlayer = event.getPlayer();
+        String playerName = enteringPlayer.getName();
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (player != enteringPlayer) {
+                if (!player.isSleeping()) {
+                    player.sendMessage("Get in bed " + playerName + "!!");
+                }
+            }
         }
     }
 }
