472032fe85e16a872335a81e9a52a7a6f335e8a1
Cory Torode <cowtorode@gmail.com>
2025-01-15 18:15:58 -0500

    Registered some commands and events

--- src/main/java/com/galactic/GalacticCore.java
+++ src/main/java/com/galactic/GalacticCore.java
@@ -1,10 +1,19 @@
 package com.galactic;
 
+import com.galactic.commands.LockCommand;
+import com.galactic.commands.MemCommand;
+import com.galactic.events.ChatManager;
 import com.galactic.events.DeathListener;
 import com.galactic.events.JoinListener;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Optional;
@@ -24,10 +33,31 @@ public class GalacticCore extends JavaPlugin
     {
     }
 
+    private <T extends Listener> T registerEvent(T listener)
+    {
+        getServer().getPluginManager().registerEvents(listener, this);
+        return listener;
+    }
+
+    private void registerCommands()
+    {
+        LifecycleEventManager<Plugin> manager = this.getLifecycleManager();
+
+        manager.registerEventHandler(LifecycleEvents.COMMANDS, event -> {
+            final Commands commands = event.registrar();
+
+            commands.register("lock", registerEvent(new LockCommand()));
+            commands.register("mem", new MemCommand());
+        });
+    }
+
     private void registerEvents()
     {
-        getServer().getPluginManager().registerEvents(new DeathListener(), this);
-        getServer().getPluginManager().registerEvents(new JoinListener(this), this);
+        PluginManager manager = getServer().getPluginManager();
+
+        manager.registerEvents(new ChatManager(this), this);
+        manager.registerEvents(new DeathListener(), this);
+        manager.registerEvents(new JoinListener(this), this);
     }
 
     @Override
@@ -35,6 +65,7 @@ public class GalacticCore extends JavaPlugin
     {
         OWNER = Bukkit.getOfflinePlayer(UUID.fromString("97a2ce01-4159-498a-a242-06604a766019"));
 
+        registerCommands();
         registerEvents();
     }
 
