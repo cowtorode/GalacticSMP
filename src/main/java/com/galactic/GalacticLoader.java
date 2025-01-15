7adb30cbadf51f6f825c72a0dd6a394d6d4c32a6
Cory Torode <cowtorode@gmail.com>
2025-01-15 18:16:54 -0500

    Added papermc loader

---
+++ src/main/java/com/galactic/GalacticLoader.java
@@ -0,0 +1,13 @@
+package com.galactic;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+
+public class GalacticLoader implements PluginLoader
+{
+    @Override
+    public void classloader(PluginClasspathBuilder builder)
+    {
+
+    }
+}
